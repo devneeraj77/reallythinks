6317b88305191f421cdaa9d828d6756cad980769
Neeraj Rekwar (Programmer) <dev.neerajrekwar@gmail.com>
2025-01-18 20:32:26 +0000

    fdsa

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { auth } from "@/auth";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { useRouter } from "next/navigation";
 
 export default async function ProfilePage() {
@@ -16,17 +17,28 @@ export default async function ProfilePage() {
 
   return (
     <div className="max-w-lg mx-auto p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md">
-      fdsafa
+      <span className="hidden text-sm sm:inline-flex">
+        {session.user.email}
+      </span>
       <div className="flex items-center space-x-4">
-        <img
-          src={session.user.image || "/default-avatar.png"}
-          alt="Profile Picture"
-          className="w-16 h-16 rounded-full"
-        />
-        <div>
-          <h2 className="text-xl font-semibold text-gray-800 dark:text-white">
-            {session.user.name || "Unknown User"}
-          </h2>
+        <Avatar className="h-8 w-8">
+          <AvatarImage
+            src={
+              session.user.image ??
+              `https://api.dicebear.com/9.x/thumbs/svg?seed=${
+                Math.floor(Math.random() * 100000) + 1
+              }&randomizeIds=true`
+            }
+            alt={session.user.name ?? ""}
+          />
+        </Avatar>
+        <div className="flex flex-col space-y-1">
+          <p className="text-sm font-medium leading-none">
+            {session.user.name}
+          </p>
+          <p className="text-muted-foreground text-xs leading-none">
+            {session.user.email}
+          </p>
         </div>
       </div>
     </div>
