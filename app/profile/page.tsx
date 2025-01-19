4ae8ebf4bf4c5fedd204531ba3767143f3d479b0
Neeraj Rekwar (Programmer) <dev.neerajrekwar@gmail.com>
2025-01-19 06:14:29 +0000

    gfd

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,6 @@
 import { SignIn } from "@/components/auth-components";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+
 import { auth } from "auth";
 
 export default async function UserButton() {
@@ -17,35 +11,23 @@ export default async function UserButton() {
       <span className="hidden text-sm sm:inline-flex">
         {session.user.email}
       </span>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-            <Avatar className="h-8 w-8">
-              <AvatarImage
-                src={
-                  session.user.image ??
-                  `https://api.dicebear.com/9.x/thumbs/svg?seed=${
-                    Math.floor(Math.random() * 100000) + 1
-                  }&randomizeIds=true`
-                }
-                alt={session.user.name ?? ""}
-              />
-            </Avatar>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">
-                {session.user.name}
-              </p>
-              <p className="text-muted-foreground text-xs leading-none">
-                {session.user.email}
-              </p>
-            </div>
-          </DropdownMenuLabel>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <Avatar className="h-8 w-8">
+        <AvatarImage
+          src={
+            session.user.image ??
+            `https://api.dicebear.com/9.x/thumbs/svg?seed=${
+              Math.floor(Math.random() * 100000) + 1
+            }&randomizeIds=true`
+          }
+          alt={session.user.name ?? ""}
+        />
+      </Avatar>
+      <div className="flex flex-col space-y-1">
+        <p className="text-sm font-medium leading-none">{session.user.name}</p>
+        <p className="text-muted-foreground text-xs leading-none">
+          {session.user.email}
+        </p>
+      </div>
     </div>
   );
 }
