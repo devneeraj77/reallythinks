0aeac7eaba497de6b9a77cd1d29e1890a17cd6ac
Neeraj Rekwar (Programmer) <dev.neerajrekwar@gmail.com>
2025-01-20 14:12:19 +0000

    sd

---
+++ app/@[username]/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ username: string }>;
+}) {
+  const { username } = await params;
+  return <h1>Blog Post: {username}</h1>;
+}
