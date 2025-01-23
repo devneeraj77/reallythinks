a6d46f9670501852ebace7b16c09f2dacea08bd8
Neeraj Rekwar (Programmer) <dev.neerajrekwar@gmail.com>
2025-01-23 18:33:22 +0000

    gs

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,25 +1,30 @@
 import { NextResponse } from "next/server";
 import redis from "@/lib/redis";
+import { z } from "zod";
 import { MessageSchema } from "@/lib/schemas/message";
 
 export async function POST(request: Request) {
-  const body = await request.json();
-
   try {
+    const body = await request.json();
     const message = MessageSchema.parse(body);
-    await redis.rpush(`messages:${message.receiver}`, JSON.stringify(message));
-    return NextResponse.json({ success: true });
-  } catch (error) {
-    let errorMessage = "An unknown error occurred";
 
-    // Check if the error is an instance of Error
-    if (error instanceof Error) {
-      errorMessage = error.message;
+    // Check if the user exists in Redis
+    const userExists = await redis.exists(`user:${message.receiver}`);
+    if (!userExists) {
+      return NextResponse.json({ error: "User not found." }, { status: 404 });
     }
 
+    await redis.rpush(`messages:${message.receiver}`, JSON.stringify(message));
+    return NextResponse.json({ success: true, message: "Message sent!" });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json({ error: error.errors }, { status: 400 });
+    }
+
+    // Check if the error is an instance of Error
     return NextResponse.json(
-      { success: false, error: errorMessage },
-      { status: 400 }
+      { error: "An error occurred while sending the message." },
+      { status: 500 }
     );
   }
 }
