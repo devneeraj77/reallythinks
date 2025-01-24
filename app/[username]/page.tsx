4bb916411ef719d4db9f7d66c3efd6efe6212a67
Neeraj Rekwar (Programmer) <dev.neerajrekwar@gmail.com>
2025-01-24 07:51:17 +0000

    gfdsa

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,16 +1,16 @@
-import SendMessage from "@/components/SendMessage"; // Adjust path if necessary
+import SendMessage from "../../components/SendMessage"; // Adjust relative path as necessary
 
 export default async function UserPage({
   params,
 }: {
-  params: Promise<{ receiver: string }>;
+  params: Promise<{ username: string }>;
 }) {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">
-        Send an Anonymous Message to {(await params).receiver}
+        Send an Anonymous Message to {(await params).username}
       </h1>
-      <SendMessage receiver={(await params).receiver} />
+      <SendMessage receiver={(await params).username} />
     </div>
   );
 }
