0a5639eb92ca005cb2b4f3d5c3e6f86eb3ec7d65
Neeraj Rekwar (Programmer) <dev.neerajrekwar@gmail.com>
2025-01-24 10:12:56 +0000

    gsf

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,4 +1,6 @@
+import redis from "@/lib/redis";
 import SendMessage from "../../components/SendMessage"; // Adjust the relative path as necessary
+import { NextResponse } from "next/server";
 
 export default async function UserPage({
   params,
@@ -6,6 +8,13 @@ export default async function UserPage({
   params: Promise<{ username: string }>;
 }) {
   const { username } = await params;
+
+  console.log("Validating receiver existence...");
+  const userExists = await redis.exists(`user:${username}`);
+  if (!userExists) {
+    console.error("Receiver not found:", username);
+    return NextResponse.json({ error: "Receiver not found." }, { status: 404 });
+  }
 
   return (
     <div className="p-4">
