eda7404cc99c5b8d49a1b2e45f17db41a90433d8
Neeraj Rekwar (Programmer) <dev.neerajrekwar@gmail.com>
2025-01-24 13:36:28 +0000

    gs

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,29 +1,10 @@
 import SendMessage from "@/components/SendMessage"; // Adjust the path as necessary
-import redis from "@/lib/redis";
-
 export default async function UserPage({
   params,
 }: {
   params: Promise<{ username: string }>;
 }) {
   const { username } = await params; // Normalize username
-
-  // Fetch the user from Upstash Redis
-  const user = await redis.hgetall(`user:${username}`);
-
-  // If user is not found, render a custom message or a 404 page
-  if (!user) {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen text-center">
-        <h1 className="text-4xl font-bold text-red-600">
-          User could not be found
-        </h1>
-        <p className="mt-2 text-lg text-gray-700">
-          The username `&quot;{username}`&quot; does not exist in our database.
-        </p>
-      </div>
-    );
-  }
 
   return (
     <div className="p-4 max-w-md mx-auto">
