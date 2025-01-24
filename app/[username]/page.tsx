15d2a14fc1ab71fb432ee159323a82a54d0186a6
Neeraj Rekwar (Programmer) <dev.neerajrekwar@gmail.com>
2025-01-24 12:31:42 +0000

    gfdsfd

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,31 +1,25 @@
-// /app/[username]/page.tsx
-import { notFound } from "next/navigation";
 import SendMessage from "@/components/SendMessage";
+import redis from "@/lib/redis";
+
+import { notFound } from "next/navigation";
 
 export default async function UserPage({
   params,
 }: {
-  params: Promise<{ username: string }>;
+  params: { username: string };
 }) {
-  const username = (await params).username.toLowerCase();
+  const username = params.username.toLowerCase();
 
-  // Fetch user validation from the API
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/check-user?username=${username}`
-  );
-
-  if (!res.ok) {
-    notFound(); // Redirect to a 404 page if the user doesn't exist
+  const userExists = await redis.exists(`user:${username}`);
+  if (!userExists) {
+    notFound();
   }
 
-  const { message } = await res.json();
-
   return (
-    <div className="p-4 max-w-md mx-auto">
+    <div className="p-4 max-w-lg mx-auto">
       <h1 className="text-2xl font-bold text-center">
         Send an anonymous message to {username}
       </h1>
-      <p className="text-center text-gray-500">{message}</p>
       <SendMessage receiver={username} />
     </div>
   );
