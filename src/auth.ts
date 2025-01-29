09d6296b4447fd4bc08bcbd66f40d24fa8f5292a
Neeraj Rekwar (Programmer) <dev.neerajrekwar@gmail.com>
2025-01-29 07:06:25 +0000

    ggf

--- src/auth.ts
+++ src/auth.ts
@@ -61,7 +61,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       },
     }),
   ],
-  basePath: "/auth",
   session: { strategy: "jwt" },
   callbacks: {
     authorized({ request, auth }) {
