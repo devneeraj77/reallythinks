75bb3e610034b325efc11971991fd4e1ee0b7454
Neeraj Rekwar (Programmer) <dev.neerajrekwar@gmail.com>
2025-03-15 09:29:19 +0000

    FDAS

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,24 +16,27 @@ export const metadata: Metadata = {
   description:
     "Reallythinks, the safest way to send and receive anonymous messages. Share your thoughts without revealing your identity and connect with others through completely private and secure messaging. Whether it's a confession, feedback, or just a fun surprise, your anonymity is guaranteed!",
   openGraph: {
-    title: "Discover your hidden messages anywhere with Reallythinks presents.",
+    title: "Reallythinks - find secret messages",
     description:
-      "Reallythinks is a web application that allows users to send anonymous messages.",
+      "Discover your hidden messages anywhere with Reallythinks presents.",
     type: "website",
     url: "https://reallythinks.vercel.app/",
     siteName: "Reallythinks",
     images: {
-      url: `${profileImage}`,
+      url: "https://images.unsplash.com/photo-1638376776402-9a4b75fe21bb?q=80&w=1577&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      width: 800,
+      height: 600,
+      alt: "My custom alt",
     },
   },
   twitter: {
     card: "summary_large_image",
-    description: "",
+    description: "hllow",
     siteId: "7774646541651411874854",
     creator: "@uneerajrekwar",
     creatorId: "7774646541651411874854",
     title: "Reallythinks is anonymous.",
-    images: `${profileImage}`,
+    images: [`${profileImage}`],
   },
 };
 
